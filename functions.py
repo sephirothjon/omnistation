a4b2af81d5199acf87246a1d2041b9b8992b8205
sephirothjon <62864229+sephirothjon@users.noreply.github.com>
2024-01-13 00:28:40 -0500

    functions.py

--- functions.py
+++ functions.py
@@ -16,7 +16,7 @@ def tictoc(func):
         t1=time.time()
         func()
         t2=time.time()-t1
-        print(f'(func.__name__) ran in {t2} secs)')
+        print(f'({func.__name__}) ran in {t2} secs)')
     return wrapper
 
 @tictoc
